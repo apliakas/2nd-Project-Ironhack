e6d8670e3b01e47634a54c18b879b31a2d5a9448
Rich Alossi <alossix@gmail.com>
2020-11-14 15:07:43 +0100

    Add basic routing

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
+require("dotenv").config();
+
 const path = require("path");
-require("dotenv").config({
-  path: require("path").resolve(__dirname, "../.env"),
-});
+const hbs = require("hbs");
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+const app = express();
+
+require("./routes/routes.js");
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+const index = require("./routes/routes");
+app.use("/", index);
+
+app.use(logger("dev"));
+
+app.listen(3000);
---
+++ routes/routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const PORT = process.env.PORT;
+
+router.get("/", (req, res) => {});
+
+module.exports = router;
