3c1fb04b588f8e7f356e6cc3a24f0fb37d086949
Rich Alossi <alossix@gmail.com>
2020-11-14 17:05:10 +0100

    Add signup option

--- app.js
+++ app.js
@@ -5,20 +5,28 @@ const hbs = require("hbs");
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
-
-app.use(morgan("dev"));
 
 require("./routes/routes.js");
 require("./configs/sessions.config")(app);
 require("./configs/db.config");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-const index = require("./routes/routes");
-app.use("/", index);
+app.use("/", require("./routes/routes"));
 
-app.listen(3000);
+app.listen(process.env.PORT, () =>
+  console.log(`App running on port ${process.env.PORT}`)
+);
+
+module.exports = app;
--- routes/routes.js
+++ routes/routes.js
@@ -8,8 +8,44 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 
 router.get("/", (req, res) => {
-  console.log();
-  res.render("index");
+  res.render("index", {
+    userInSession: req.session.user,
+  });
+});
+
+router.get("/signup", (req, res) => res.render("signup"));
+
+router.post("/signup", (req, res) => {
+  const {
+    username,
+    email,
+    firstName,
+    lastName,
+    favArtist,
+    password,
+  } = req.body;
+  bcrypt
+    .hash(password, saltRounds)
+    .then((hashedPassword) => {
+      User.create({
+        username,
+        email,
+        firstName,
+        lastName,
+        favArtist,
+        passwordHash: hashedPassword,
+      })
+        .then((user) => {
+          req.session.currentUser = user;
+          res.redirect("/user-profile");
+        })
+        .catch((err) => {
+          res.render("auth/signup", {
+            errorMessage: "The username or email address is already in use.",
+          });
+        });
+    })
+    .catch((err) => console.error(err));
 });
 
 module.exports = router;
